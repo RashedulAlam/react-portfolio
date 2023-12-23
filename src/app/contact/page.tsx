001e4615062dd04126edea5c521a6bacf49cc900
Rashedul-Alam-Rudra <rashedulalam.iit.du@gmail.com>
2023-12-23 14:25:07 +0200

    addded contact form page and component

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import ContactForm from "@/components/contact/ContactForm";
+import React from "react";
+
+const page = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <ContactForm />
+    </main>
+  );
+};
+
+export default page;
