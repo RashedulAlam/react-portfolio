8081f45ea678510e13fd2835ad2476e4ddf71dfd
Rashedul-Alam-Rudra <rashedulalam.iit.du@gmail.com>
2024-01-14 19:18:12 +0200

    updated resume and fixed typo

--- src/config/hero.tsx
+++ src/config/hero.tsx
@@ -1,6 +1,6 @@
 export const HERO_SUMMARY = {
   name: "Hi! I'm Rashedul",
-  shortIntro: `I am a <strong> Full Stack Developer </strong>. My specialization is on <strong> .NET, JavaScript and Cloud </strong> based technolonogies. I have 6+ years of 
+  shortIntro: `I am a <strong> Full Stack Developer </strong>. My specialization is on <strong> .NET, JavaScript and Cloud </strong> based technolongies. I have 6+ years of 
     experience in <strong> designing, developing and maintaining </strong> software technologies`,
   moreText: "(Read More)",
   cvUrl: "/artifacts/cv-rashedul-alam.pdf",
